4a8e53ea603458b476135fb44b85645f5eb55aa7
MiX1 <47393468+Sobhix1@users.noreply.github.com>
2019-04-03 13:08:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["380810113813970944"]
+const developers = ["448900716220907531"]
 const adminprefix = "^";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
